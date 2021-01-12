4fbaf76af8e816b3b526aecb29088e75065abdf8
junghyun2006 <jung.hyun.park516@gmail.com>
2021-01-12 15:15:31 -0500

    finish receiveSinglePokemon reducer

---
+++ pokedex_part2/frontend/util/api_util.js
@@ -0,0 +1,14 @@
+export const fetchAllPokemon = () => {
+  return $.ajax({
+    method: "GET",
+    url: "/api/pokemon"
+  })
+}
+
+export const requestSinglePokemon = (id) => {
+  return $.ajax({
+    method: "GET",
+    url: `/api/pokemon/${id}`
+  })
+}
+
